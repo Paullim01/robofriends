1e2f418f2922d5f8cf2f476ba79706ca7f483308
PaulLim <paullim007@msn.com>
2019-02-27 17:26:30 -0500

    react app search

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Leanna Graham',
+		username: "Bret",
+		email: 'someemail@jan.com'
+	},
+	{
+		id: 2,
+		name: 'Bender Rod',
+		username: "Futurama",
+		email: "Futurama@tv.com"
+	},
+	{
+		id: 3,
+		name: 'Will Smith',
+		username: "Irobot",
+		email: "wtfmovie@meh.com"
+	},
+	{
+		id: 4,
+		name: 'Big Little',
+		username: "Smart",
+		email: "bigSmart@little.com"
+	},
+	{
+		id: 5,
+		name: 'Geodude Onxy',
+		username: "Rock Brock",
+		email: "gymleader@pokemon.com"
+	},
+	{
+	    id: 6,
+	    name: 'Mrs. Dennis Schulist',
+	    username: 'Leopoldo_Corkery',
+	    email: 'Karley_Dach@jasper.info'
+	},
+	{
+	    id: 7,
+	    name: 'Kurtis Weissnat',
+	    username: 'Elwyn.Skiles',
+	    email: 'Telly.Hoeger@billy.biz'
+	},
+ 	{
+	    id: 8,
+	    name: 'Nicholas Runolfsdottir V',
+	    username: 'Maxime_Nienow',
+	    email: 'Sherwood@rosamond.me'
+	},
+	{
+	    id: 9,
+	    name: 'Glenna Reichert',
+	    username: 'Delphine',
+	    email: 'Chaim_McDermott@dana.io'
+	},
+	{
+	    id: 10,
+	    name: 'Clementina DuBuque',
+	    username: 'Moriah.Stanton',
+	    email: 'Rey.Padberg@karina.biz'
+	}	
+]
